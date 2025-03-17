487cac88575f7b08d6f20a86ab3d39f2a9a4675e
unknown <ag_antro@hotmail.com>
2025-03-16 21:12:08 -0300

    last commit

--- scripts/script-projects.js
+++ scripts/script-projects.js
@@ -1,11 +1,17 @@
 import { translations, projects } from "./script-data.js";
-import { currentLanguage } from "./script-translations.js";
 
-export function renderProjects() {
+function toggleLanguage() {
+    currentLanguage = (currentLanguage === 'es') ? 'en' : 'es';
+    updateUI();
+}
+
+export function renderProjects(language = "en") {
     const container = document.getElementById('projects-container');
     container.innerHTML = '';
 
-    const translatedProjects = translations[currentLanguage].projectsData;
+    const translatedProjects = translations[language].projectsData;
+
+    console.log(translatedProjects)
 
     translatedProjects.forEach((project, index) => {
         const projectCard = document.createElement('div');
@@ -15,10 +21,16 @@ export function renderProjects() {
             <img src="${projects[index].imageUrl}" alt="${project.title}" loading="lazy">
             <h3>${project.title}</h3>
             <p>${project.description} ${project.features}</p>
-            <a href="${projects[index].linkDeploy}" target="_blank">${translations[currentLanguage].viewDeploy}</a>
-            <a href="${projects[index].linkGitHub}" target="_blank">${translations[currentLanguage].viewCode}</a>
+            <a href="${projects[index].linkDeploy}" target="_blank">${translations[language].viewDeploy}</a>
+            <a href="${projects[index].linkGitHub}" target="_blank">${translations[language].viewCode}</a>
+            <button>toggle</button>
         `;
 
         container.appendChild(projectCard);
     });
 }
+
+if (typeof renderProjects === "function") {
+    renderProjects();
+}
+
